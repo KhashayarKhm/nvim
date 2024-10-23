e9410d09ed1bafb5941405b7bb3941124c9435db
KhashayarKhm <khashayar.khm@protonmail.com>
2024-10-23 17:12:20 +0330

    feat(null-ls): add custom ruff formatter and prettier_slim_d

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,12 +1,39 @@
 local status, null_ls = pcall(require, "null-ls")
+local methods = require("null-ls.methods")
+local helpers = require("null-ls.helpers")
+
 if (not status) then return end
+
+local function ruff_fix()
+    return helpers.make_builtin({
+        name = "ruff",
+        meta = {
+            url = "https://github.com/charliermarsh/ruff/",
+            description = "An extremely fast Python linter, written in Rust.",
+        },
+        method = methods.internal.FORMATTING,
+        filetypes = { "python" },
+        generator_opts = {
+            command = "ruff",
+            -- args = { "--fix", "-e", "-n", "--stdin-filename", "$FILENAME", "-" },
+            args = { "format", "--stdin-filename", "$FILENAME", "-" },
+            to_stdin = true
+        },
+        factory = helpers.formatter_factory
+    })
+end
 
 null_ls.setup({
   sources = {
+    -- null_ls.builtins.formatting.black,
+    -- null_ls.builtins.formatting.ruff.with({
+    --   arg = { "format", "$FILENAME" },
+    -- }),
+    ruff_fix(),
+    null_ls.builtins.formatting.prettier_d_slim,
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
-    }),
-    null_ls.builtins.diagnostics.fish
+    })
   }
 })
 
